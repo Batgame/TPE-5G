4e00a49d3e4c697fe25ef3fad8b335ae271c0811
Batgame <batgen42@gmail.com>
2018-09-17 23:39:17 +0200

    Python v1.4

--- server.py
+++ server.py
@@ -1,38 +1,49 @@
-import socket, sys
+import socket, sys, threading
 
 host = '192.168.1.11'
 port= 8080
-counter = 0
+
+class ThreadClient(threading.Thread):
+	def __init__(self, conn):
+		threading.Thread.__init__(self)
+		self.connexion = conn
+
+	def run(self):
+		nom = self.getName()
+		while 1:
+			MsgClient = self.connexion.recv(1024).decode('Utf-8')
+			if not MsgClient or MsgClient.upper() == 'FIN':
+				break
+			message = '%s> %s' (nom, MsgClient)
+			print(message)
+
+			for cle in conn_client:
+				if cle != nom:
+					conn_client[cle].send(message.encode('Utf-8'))
+
+		self.connexion.close()
+		del conn_client[nom]
+		print('Client %s déconnecté' % nom)
 
 MainSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 try:
-	MainSocket.bind((host, port)) #Liaison du socket à l'adresse et au port
+	MainSocket.bind((host, port))
 
 except socket.error:
-	print("[*] La liaison du socket à l'adresse à échoué.")
-	sys.exit
+	print("[*] La liaison du socket à l'adresse a échoué")
+	sys.exit()
+print('[*] Serveur prêt, en attente de requêtes...')
+MainSocket.listen(5)
 
+conn_client = {}
 while 1:
-	print('[*] Serveur prêt, en attente de requêtes...')
-	MainSocket.listen(2) #Attente d'une connexion entrante
+	connexion, adresse = MainSocket.accept()
+	th_C = ThreadClient(connexion)
+	th_C.start()
 
-	connexion, adresse = MainSocket.accept() #Connexion entrante accepté
-	counter +=1
-	print('[*] Client connecté, adresse IP %s, port %s' % (adresse[0], adresse[1]))
+	it = th_C.getName()
+	conn_client[it] = connexion
+	print('Client %s connecté, adresse IP %s, port %s' % (it, adresse[0], adresse[1]))
 
-	MsgServeur = "Vous êtes connecté au serveur Batgame. Envoyer vos messages."
-	connexion.send(MsgServeur.encode('Utf-8')) #Le message de bienvenue est envoyé au client (encodé en UTF8)
-	MsgClient = connexion.recv(1024).decode('Utf-8') #Si reception d'un message du client, le decoder
-	while 1:
-		print('Client> ', MsgClient)
-		if MsgClient.upper() == "FIN" or MsgClient == "":
-			break
-		MsgServeur = input('Serveur> ')
-		connexion.send(MsgServeur.encode('Utf-8'))
-		MsgClient = connexion.recv(1024).decode('Utf-8')
-
-	connexion.send('Fin'.encode('Utf-8'))
-	print('[*] Connexion interrompue.')
-	connexion.close()
-
+	msg = 'Vous êtes connecté, envoyez vos messages'
+	connexion.send(msg.encode('Utf-8'))
